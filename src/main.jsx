fe65f5afe37cc4c9845232ab223813453940e0a2
Aiden Murphy <aidenjmurph@gmail.com>
2025-05-13 22:08:56 -0700

    my battery is dying

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,42 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+import './App.css';
 
 import App from './Home.jsx';
 import Forum from './Forum.jsx';
 import Demo from './Demo.jsx';
 import Downloads from './Downloads.jsx';
 
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!clerkPubKey) {
+  throw new Error('Missing Clerk Publishable Key');
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/forum" element={<Forum />} />
-        <Route path="/demo" element={<Demo />} />
-        <Route path="/downloads" element={<Downloads />} />
-      </Routes>
-    </Router>
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/sign-in/*" element={<SignIn routing="path" path="/sign-in" />} />
+          <Route path="/sign-up/*" element={<SignUp routing="path" path="/sign-up" />} />
+          <Route path="/forum" element={
+            <>
+              <SignedIn>
+                <Forum />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>            </>} 
+          />
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/downloads" element={<Downloads />} />
+        </Routes>
+      </Router>
+    </ClerkProvider>
   </StrictMode>
 );
